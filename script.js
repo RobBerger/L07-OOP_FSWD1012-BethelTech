619d5994caa5f0a39d68d0ffddf8f302e4e56217
berger <robertberger.dev@gmail.com>
2023-01-04 17:42:16 -0500

    added another course example

--- script.js
+++ script.js
@@ -16,58 +16,78 @@
 //     }
 // }
 
-class Contact {
-    constructor(first, last, phone) {
-        this.firstName = first;
-        this.lastName = last; 
-        this.phone = phone;
-    }
+// class Contact {
+//     constructor(first, last, phone) {
+//         this.firstName = first;
+//         this.lastName = last; 
+//         this.phone = phone;
+//     }
 
-    info() {
-        console.log("My name is " + this.firstName + " " + this.lastName + " and my phone number is " + this.phone + ".");
-    }
-}
+//     info() {
+//         console.log("My name is " + this.firstName + " " + this.lastName + " and my phone number is " + this.phone + ".");
+//     }
+// }
 
-let jed = new Contact("Jed", "Walker", "123-456-7890");
-jed.info();
+// let jed = new Contact("Jed", "Walker", "123-456-7890");
+// jed.info();
 
-class Vehicle {
-    constructor(clr, numWheels, fuelLevel, numDoors) {
-        this.color = clr;
-        this.numOfWheels = numWheels;
-        this.fuelLvl = fuelLevel;
-        this.doors = numDoors;
-        this.state = "stopped";
-    }
+// class Vehicle {
+//     constructor(clr, numWheels, fuelLevel, numDoors) {
+//         this.color = clr;
+//         this.numOfWheels = numWheels;
+//         this.fuelLvl = fuelLevel;
+//         this.doors = numDoors;
+//         this.state = "stopped";
+//     }
 
-    go() {
-        this.state = "moving";
-    }
+//     go() {
+//         this.state = "moving";
+//     }
 
-    stop() {
-        this.state = "stopped";
-    }
+//     stop() {
+//         this.state = "stopped";
+//     }
 
-    status() {
-        if (this.state === "moving") {
-            alert("Here we go!");
-        } else {
-            alert("Did you start the Vehicle?");
-        }
-    }
+//     status() {
+//         if (this.state === "moving") {
+//             alert("Here we go!");
+//         } else {
+//             alert("Did you start the Vehicle?");
+//         }
+//     }
 
-    message() {
-        console.log("My vehicle is a " + this.color + " " + this.doors + " door SUV with " + this.numOfWheels + " wheels and a fuel level of " + this.fuelLvl + " of a tank.")
-    }
-}
+//     message() {
+//         console.log("My vehicle is a " + this.color + " " + this.doors + " door SUV with " + this.numOfWheels + " wheels and a fuel level of " + this.fuelLvl + " of a tank.")
+//     }
+// }
 
 // initialize a new Vehicle object
-let myCar = new Vehicle("gray", 4, 0.5, 4);
+// let myCar = new Vehicle("gray", 4, 0.5, 4);
 
 // call class methods using dot notation
-myCar.go();
-myCar.status();
-myCar.message();
+// myCar.go();
+// myCar.status();
+// myCar.message();
+
+// class Course {
+//     constructor(name, timeOfClass, pointsAvailable) {
+//         this.name = name;
+//         this.timeOfClass = timeOfClass;
+//         this.pointsAvailable = pointsAvailable
+//     }
+
+//     classTime() {
+//         alert("Class will begin " + this.timeOfClass);
+//     }
+
+//     changeClassTime(time) {
+//         this.timeOfClass = time;
+//         alert("Class time has been changed to " + this.timeOfClass);
+//     }
+// }
+
+// let aClass = new Course("A Class", "Mon 1:00pm", 200);
+// aClass.changeClassTime("Mon 3:00pm");
 
 class Course {
     constructor(name, timeOfClass, pointsAvailable) {
@@ -82,9 +102,17 @@ class Course {
 
     changeClassTime(time) {
         this.timeOfClass = time;
-        alert("Class time has been changed to " + this.timeOfClass);
+        alert("Can we change class time to " + this.timeOfClass);
+    }
+
+    get time() {
+        return "Class will now be " + this.timeOfClass;
     }
 }
 
 let aClass = new Course("A Class", "Mon 1:00pm", 200);
+aClass.classTime()
 aClass.changeClassTime("Mon 3:00pm");
+
+// invoke the getter
+alert(aClass.time);
