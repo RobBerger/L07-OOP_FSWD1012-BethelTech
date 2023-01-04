2e042823694f9004a8172c4dcd11178e0a85320d
berger <robertberger.dev@gmail.com>
2023-01-04 15:06:36 -0500

    commented out car exaple

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const car = {
-    make: "VW",
-    model: "Bug",
-    color: "Orange",
-    start: function() {
-        console.log("starting the car")
-    }
-}
+// const car = {
+//     make: "VW",
+//     model: "Bug",
+//     color: "Orange",
+//     start: function() {
+//         console.log("starting the car")
+//     }
+// }
 
-const car2 = {
-    make: "Ferrari",
-    model: "812",
-    color: "Rosso Fuoco",
-    start: function() {
-        console.log("starting the car")
-    }
-}
+// const car2 = {
+//     make: "Ferrari",
+//     model: "812",
+//     color: "Rosso Fuoco",
+//     start: function() {
+//         console.log("starting the car")
+//     }
+// }
+
