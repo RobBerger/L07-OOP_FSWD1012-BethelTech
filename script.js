203511ddb8d3e8040684c62d562914417372478a
berger <robertberger.dev@gmail.com>
2023-01-04 17:18:29 -0500

    added vehicle example

--- script.js
+++ script.js
@@ -30,3 +30,41 @@ class Contact {
 
 let jed = new Contact("Jed", "Walker", "123-456-7890");
 jed.info();
+
+class Vehicle {
+    constructor(clr, numWheels, fuelLevel, numDoors) {
+        this.color = clr;
+        this.numOfWheels = numWheels;
+        this.fuelLvl = fuelLevel;
+        this.doors = numDoors;
+        this.state = "stopped";
+    }
+
+    go() {
+        this.state = "moving";
+    }
+
+    stop() {
+        this.state = "stopped";
+    }
+
+    status() {
+        if (this.state === "moving") {
+            alert("Here we go!");
+        } else {
+            alert("Did you start the Vehicle?");
+        }
+    }
+
+    message() {
+        console.log("My vehicle is a " + this.color + " " + this.doors + " door SUV with " + this.numOfWheels + " wheels and a fuel level of " + this.fuelLvl + " of a tank.")
+    }
+}
+
+// initialize a new Vehicle object
+let myCar = new Vehicle("gray", 4, 0.5, 4);
+
+// call class methods using dot notation
+myCar.go();
+myCar.status();
+myCar.message(); 
